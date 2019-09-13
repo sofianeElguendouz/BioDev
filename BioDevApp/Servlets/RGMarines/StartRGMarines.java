c28571bc884bccb7d7dd9f620a251c9170777f67
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2019-09-13 23:52:30 +0100

    add dao test for classifications enumeration

--- BioDevApp/Servlets/RGMarines/StartRGMarines.java
+++ BioDevApp/Servlets/RGMarines/StartRGMarines.java
@@ -1,11 +1,20 @@
 package RGMarines;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Connexion.ConnexionSuperUser;
+import Entities.Utilisateur;
+import Users.UtilisateurDAOImpSuperUser;
 
 
 @WebServlet("/StartRGMarines")
@@ -17,9 +26,17 @@ public class StartRGMarines extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
-			this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/RGMarines/homeMarine.jsp").forward(request, response);
-		}
+		
+		HttpSession session = request.getSession();
+		List<Classification> listClassifications = new ArrayList<Classification>();
+		ClassificationDaoImpl classif =new ClassificationDaoImpl();
+		
+		listClassifications = classif.listerClassifications(1);
+		System.out.println("Chemin d'image est : "+listClassifications.get(0).getImage());
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/RGMarines/homeMarine.jsp").forward(request, response);
+		
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
