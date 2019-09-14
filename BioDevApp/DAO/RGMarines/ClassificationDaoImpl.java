e130d8c2b3c0305258ae15c94f7a1f3bf07d9bb2
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2019-09-14 16:22:40 +0100

    add parent_id parameter to listClassifications function

--- BioDevApp/DAO/RGMarines/ClassificationDaoImpl.java
+++ BioDevApp/DAO/RGMarines/ClassificationDaoImpl.java
@@ -19,12 +19,13 @@ public class ClassificationDaoImpl {
 
 	
 	
-	public List<Classification> listerClassifications(int num_niveau) {
+	public List<Classification> listerClassifications(int num_niveau, int parent_id) {
 		
 		List<Classification> classifications= new ArrayList<Classification>();
 		Query requete = em.createNamedQuery("Classification.find", Classification.class);
 		//Query requete = em.createQuery("SELECT u FROM rgClassificationMarine u WHERE  u.num_niveau = :num_niveau");
 		requete.setParameter ("num_niveau",num_niveau);
+		requete.setParameter("parent_id", parent_id);
 		classifications = requete.getResultList();
 		//System.out.println(classifications.get(0).getNom());
 		if (classifications.isEmpty())return null; 
