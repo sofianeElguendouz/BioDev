5855ce8b783770101818e339b08850f48ade706d
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2019-09-14 16:21:32 +0100

    create embranchement jsp & servlet

--- BioDevApp/Servlets/RGMarines/Embranchement.java
+++ BioDevApp/Servlets/RGMarines/Embranchement.java
@@ -1,6 +1,9 @@
 package RGMarines;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +19,26 @@ public class Embranchement extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String i = request.getParameter("embr");
-		System.out.println("l'embranchement est : "+i);
+		
+		String numEMBR = request.getParameter("numEMBR");
+		int indice2 = Integer.valueOf(numEMBR)-1;
+		request.getSession().setAttribute("numEMBR", indice2);
+		
+		List<Classification> listClassifications2 = new ArrayList<Classification>();
+		listClassifications2 = (List<Classification>)request.getSession().getAttribute("listClassifications2");
+		
+		String pre_description2 = listClassifications2.get(indice2).getPre_description();
+		request.getSession().setAttribute("pre_description2", pre_description2);
+		String description2 = listClassifications2.get(indice2).getDescription();
+		request.getSession().setAttribute("description2", description2);
+		int nb_especes2 = listClassifications2.get(indice2).getNb_especes();
+		request.getSession().setAttribute("nb_especes2", nb_especes2);
+		String nom2 = listClassifications2.get(indice2).getNom();
+		request.getSession().setAttribute("nom2", nom2);
+		String image2 = listClassifications2.get(indice2).getImage();
+		request.getSession().setAttribute("image2", image2);
+		int id2 = listClassifications2.get(indice2).getId();
+		request.getSession().setAttribute("id2", id2);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/RGMarines/embranchement.jsp").forward(request, response);
 
 	}
--- BioDevApp/WebContent/WEB-INF/JSP/RGMarines/embranchement.jsp
+++ BioDevApp/WebContent/WEB-INF/JSP/RGMarines/embranchement.jsp
@@ -4,26 +4,21 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Embranchement</title>
+<title>${ sessionScope.nom2}</title>
 </head>
 <body>
  	<%@include file="header.jsp"  %>
- 	<h2 style="text-align:center; color:#8F8F8F">RG Marines --> Faune --> Poissons</h2><br><br>
+ 	<h2 style="text-align:center; color:#8F8F8F">RG Marines --> ${ sessionScope.nom1} --> ${ sessionScope.nom2}</h2><br><br>
  	<div>
- 	<div style="float:left;" class="text-left"><img alt="faune" src="images/RGMarines/poissons.png"></div>
+ 	<div style="float:left;" class="text-left"><img alt="faune" src="${sessionScope.image2}"></div>
  	<div style="margin-left:10%; text-align:center;">
  		<p style = "font-size:150%">
- 		
- 			Les Poissons forment un groupe de vertébrés très hétérogène, <br>
-			dont les caractéristiques communes sont la présence <br>
-			d'appareil branchial,  d'une peau écailleuse et de nageoires.<br> 
-			C'est le groupe de vertébrés le plus important.<br>
-
+ 			${sessionScope.description2}
  		</p>
  	</div>
  	</div>
  	<br>
- 	<h3 style ="text-align: center;">ACTUELLEMENT, 103 ESPÈCES DE POISSONS DE MÉDITERRANÉE  DANS LA PHOTOTHÈQUE !</h3><br><br>
+ 	<h3 style ="text-align: center;">ACTUELLEMENT, ${sessionScope.nb_especes2} ESPÈCES DE ${ sessionScope.nom2} DE MÉDITERRANÉE  DANS LA PHOTOTHÈQUE !</h3><br><br>
  	<div class="text-center">
  		<button onclick="location.href = 'Categories';" type="button" class="btn" style="color: white; background-color: #208247;">Catégories</button>
  	</div>
